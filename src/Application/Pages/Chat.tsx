bab697620fcd47b76cd67b6be1852a15d9324c69
CH1MP5T0N <philipusadriel@gmail.com>
2023-05-04 10:36:27 +0700

    Starting skeleton of chat page

--- src/Application/Pages/Chat.tsx
+++ src/Application/Pages/Chat.tsx
@@ -1,38 +1,19 @@
 import React from 'react';
 import Dashboard from '../Components/Dashboard';
-import '../Styles/HomePage.css';
+import '../Styles/Chat.css';
 
 const Home: React.FC = () => {
   return (
-    <div className="Home">
-      <header>
-        <h1>My Website</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <div className="ButtonGroup">
-          <button>Button 1</button>
-          <button>Button 2</button>
-          <button>Button 3</button>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus
-          gravida quam, sed rutrum nibh consequat vitae. Suspendisse potenti.
-          Donec at vestibulum lacus. Morbi sed quam et sem facilisis suscipit.
-        </p>
-      </main>
+    <div className="container">
+      <div className='header'>
+        
+      </div>
+      <div className='chat'>
+
+      </div>
+      <div className='footer'>
+        
+      </div>
     </div>
   );
 }
