8d666f222cbcc54ea599be6809ea6090bf69f714
CH1MP5T0N <philipusadriel@gmail.com>
2023-06-01 18:41:34 +0700

    modal functinoality, needs styling i wish i was special but IM A CREEEP

--- src/Application/Pages/Order.tsx
+++ src/Application/Pages/Order.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Components/header/layout'
 import test from '../Assets/images/future.jpg'
 function Orders() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+
+
   return (
     <div className=''>
       <Header/>
+      {isModalOpen && (
+      <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg">
+            <h1 className="text-2xl font-bold mb-4">Modal Title</h1>
+            <p onClick={closeModal}>Modal content goes here...</p>
+          </div>
+        </div>
+      )}
         <div className='flex flex-col justify-center items-center'>
-          <a href="#" data-modal-target='' className="flex flex-col items-center border border-gray-200 rounded-lg shadow md:flex-row hover:bg-black my-7 md:min-w-[50rem] max-w-md group">
+          <a href="#" onClick={openModal} className="flex flex-col items-center border border-gray-200 rounded-lg shadow md:flex-row hover:bg-black my-7 md:min-w-[50rem] max-w-md group">
             <img className="object-cover rounded-t-lg h-full w-80 md:rounded-none md:rounded-l-lg " src={test} alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
             <div className="group:hover:hidden flex items-center"> {/* Added a new div container */}
